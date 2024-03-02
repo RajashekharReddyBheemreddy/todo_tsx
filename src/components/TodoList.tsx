45221f47950b20be0f05760dd5a1c8adbafdfccf
RajashekharReddyBheemreddy <161331037+RajashekharReddyBheemreddy@users.noreply.github.com>
2024-03-02 11:47:56 -0500

    TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import TodoTypes from "../todo";
 import TodoService from "../TodoService";
 import { FaEdit, FaCheck } from "react-icons/fa";
